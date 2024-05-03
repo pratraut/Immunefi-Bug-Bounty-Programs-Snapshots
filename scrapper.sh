3bd9049363621c5467aa75275f0cdb9d8ca878a3
Prathamesh Raut <pratham.raut07@gmail.com>
2024-05-03 16:11:47 +0530

    Update
    Projects added or unpaused:
    
    Projects removed or paused:
    
    Projects updated their program:
    #AlphaVentureDAO #StaderforPolygon #aave #acala #aera #alchemix #alongside #angleprotocol #ankr #apecoinmainnet #arbitrum #astarnetwork #aurafinance #avalanche #axelarnetwork #badger #balancer #basilisk #beanstalk #beethovenx #benddao #benqi #betafinance #bitflow #blackwing #blockwallet #bobanetwork #bonfida #burrow #buttonwood #c3 #carbonbancor #celer #chainlink #composablefinance #connext #davos #debridge #decentraland #defisaver #defly #degate #driftprotocol #drips #ebtc #eco #econia #eigenlayer #elixirprotocol #enzymefinance #ethena #etherfi #exactly #extrafinance #filecoin #flamingofinance #fluxfinance #folksfinance #gainsnetwork #gearbox #gmx #gnosischain #goldfinch #hakkafinance #hathornetwork #hydradx #hyperlane #icon #illuvium #indexcoop #instadapp #interlay #ipor #kadena #keep3rnetwork #kiln #kwenta #layerzero #lido #lidoonpolygon #lifi #linea #liquidswap #listadao #lybrafinance #magpiexyz #makerdao #mantlelsp #maple #marinade #mars #metronome #moonbeamnetwork #moonwell #morpho #mountainprotocol #myntandzero #nayms #nftx #nomad #notional #oasis #oceanprotocol #olympus #ondofinance #optimism #opyngamma #opynsqueeth #orca #orderlynetwork #origindefi #pact #pancakeswap #pendle #perennial #perpetual #pnetwork #pods #polygon #polygonzkevm #polymarket #portfinance #pstake #pstakeoncosmos #pushprotocol #qblockchain #radiant #raydium #reaperfarm #reffinance #renzoprotocol #reserve #resonate #revert #revest #rhinofi #ribbon #rocketpool #rysk #scroll #segmentfinance #sei #sherlock #silofinance #smardex #sovryn #sparklend #spookyswap #spool #ssvnetwork #stackingdao #stacks #staderforbnb #staderforeth #stakewise #starkex #starknet #stellar #stellaswap #sturdy #summerfi #superfluid #superform #sushiswap #swappi #sweateconomy #swell #symbiosis #thegraph #thesandbox #thorchain #thresholdnetwork #thusd #timelessandbunni #tinyman #tinymanv2 #tokemak #toucan #traderjoe #tranchess #utix #vaultcraft #velodromefinance #waymont #wepiggy #woofi #wormhole #yearnfinance #zksync #zksyncera #zodiac
    
    Boost Projects added or unpaused:
    
    Boost Projects removed or paused:
    
    Boost Projects updated their program:

--- scrapper.sh
+++ scrapper.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+start_time=`date +%s`
 echo "#################################`date`#######################################\n"
 
 # Get NEXT_DATA in JSON format for bug bounties
 NEXT_DATA=$(curl -s https://immunefi.com/bug-bounty/ | grep -o "<script id=\"__NEXT_DATA__\" type=\"application/json\">.*</script>" | grep -o "{.*}" | jq)
 
 # Get bounties in a variable
-projects=$(echo -E "$NEXT_DATA" | jq '.props.pageProps.bounties')
+projects=$(echo "$NEXT_DATA" | jq '.props.pageProps.bounties')
 
 # Create projects.json and boost_projects.json files if not exist
 touch ./projects.json
 
 # Let's see if new projects were added or paused
 cat ./projects.json | jq -r '.[].project' | sort > prev_projects_name.txt
-echo -E "$projects" | jq -r '.[].project' | sort > current_projects_name.txt
+echo "$projects" | jq -r '.[].project' | sort > current_projects_name.txt
 
 # Paused or Removed
 paused_programs=$(comm -23 ./prev_projects_name.txt ./current_projects_name.txt | sed 's/^/#/' | sed -r 's/\s+//g' | xargs)
@@ -25,16 +26,18 @@ added_programs=$(comm -13 ./prev_projects_name.txt ./current_projects_name.txt |
 # rm ./current_projects_name.txt
 
 # Save current bounties
-echo -E "$projects" > projects.json
+echo -e "$projects" > projects.json
 
 # Get buildId
 buildId=$(echo -E "$NEXT_DATA" | jq -r '.buildId')
+echo "Bounty Build ID: $buildId"
 
 # Create folder if not exist
 test -d ./project || mkdir ./project
 
 # Get how many bounties are
 bounties_length=$(echo -E "$projects" | jq length)
+echo "Bounties Length: $bounties_length"
 
 for ((c = 0; c <= $bounties_length - 1; c++)); do
 	# Get project's name
@@ -57,7 +60,7 @@ for ((c = 0; c <= $bounties_length - 1; c++)); do
 	if [ "$name_received" = "$name" ]; then
 
 		# All good!
-		echo -E "$PROJECT_DATA" | jq 'del(.pageProps.project.vault)' > ./project/$name.json
+		echo "$PROJECT_DATA" | jq 'del(.pageProps.project.vault)' > ./project/$name.json
 		#Print DONE
 		echo -E "Scanned: $name [`expr $c + 1`/$bounties_length]"
 		sleep .3
@@ -84,7 +87,7 @@ touch ./boost_projects.json
 
 # Let's see if new projects were added or paused
 cat ./boost_projects.json | jq -r '.[].project' | sort > prev_boost_projects_name.txt
-echo -E "$boost_projects" | jq -r '.[].project' | sort > current_boost_projects_name.txt
+echo "$boost_projects" | jq -r '.[].project' | sort > current_boost_projects_name.txt
 
 # Paused or Removed
 paused_boost_programs=$(comm -23 ./prev_boost_projects_name.txt ./current_boost_projects_name.txt | sed 's/^/#/' | sed -r 's/\s+//g' | xargs)
@@ -96,20 +99,20 @@ added_boost_programs=$(comm -13 ./prev_boost_projects_name.txt ./current_boost_p
 # rm ./current_boost_projects_name.txt
 
 # Save current bounties
-echo -E "$boost_projects" > boost_projects.json
+echo -e "$boost_projects" > boost_projects.json
 
 # Get buildId
-buildIdBoost=$(echo -E "$NEXT_DATA_BOOST" | jq -r '.buildId')
+buildIdBoost=$(echo "$NEXT_DATA_BOOST" | jq -r '.buildId')
 
 # Create folder if not exist
 test -d ./boost_project || mkdir ./boost_project
 
 # Get how many bounties are
-boost_bounties_length=$(echo -E "$boost_projects" | jq length)
+boost_bounties_length=$(echo "$boost_projects" | jq length)
 
 for ((c = 0; c <= $boost_bounties_length - 1; c++)); do
 	# Get project's name
-	name=$(echo -E "$boost_projects" | jq -r .[$c].id)
+	name=$(echo "$boost_projects" | jq -r .[$c].id)
 	if [[ -z $name ]]; then
 		echo -E "ERROR: Empty Name for index [$c/$boost_bounties_length]"
 		exit
@@ -128,7 +131,7 @@ for ((c = 0; c <= $boost_bounties_length - 1; c++)); do
 	if [ "$name_received" = "$name" ]; then
 
 		# All good!
-		echo -E "$PROJECT_DATA" | jq 'del(.pageProps.project.vault)' > ./boost_project/$name.json
+		echo "$PROJECT_DATA" | jq 'del(.pageProps.project.vault)' > ./boost_project/$name.json
 		#Print DONE
 		echo -E "Scanned: $name [`expr $c + 1`/$boost_bounties_length]"
 		sleep .3
@@ -142,20 +145,24 @@ done
 
 
 # If there are any changes, commit them.
-if [[ -z $(git status -s | grep -o 'project/.*') ]]; then
+if [[ -z $(git status -s | ggrep -oP 'project/.*') ]]; then
 	echo "Nothing changed"
 else
 
 	# added_qty=$(echo "$added_programs" | sed '/^\s*$/d' | wc -l)
 	# paused_qty=$(echo "$paused_programs" | sed '/^\s*$/d' | wc -l)
-	projects_changed=$(git status -s | grep -o '(?<=M project\/).*(?=\.json)' | sed 's/^/#/' | xargs)
-	boost_projects_changed=$(git status -s | grep -o '(?<=M boost_project\/).*(?=\.json)' | sed 's/^/#/' | xargs)
+	projects_changed=$(git status -s | ggrep -oP '(?<=M project\/).*(?=\.json)' | sed 's/^/#/' | xargs)
+	boost_projects_changed=$(git status -s | ggrep -oP '(?<=M boost_project\/).*(?=\.json)' | sed 's/^/#/' | xargs)
 	# updated_qty=$(git status -s | grep -o -P '(?<=M project\/).*(?=\.json)' | sed '/^\s*$/d' | wc -l)
 
+	# git status
+	git_status=$(git status -s)
+	echo -e "Git Status:\n$git_status"
+
 	# Commit message
-	echo -E "\n"
-	mg=$(echo -E "Update\n\nProjects added or unpaused:\n$added_programs\nProjects removed or paused:\n$paused_programs\nProjects updated their program:\n$projects_changed\n\nBoost Projects added or unpaused:\n$added_boost_programs\nBoost Projects removed or paused:\n$paused_boost_programs\nBoost Projects updated their program:\n$boost_projects_changed")
-	echo -E "$mg"
+	echo -e "\n"
+	mg=$(echo -e "Update\n\nProjects added or unpaused:\n$added_programs\nProjects removed or paused:\n$paused_programs\nProjects updated their program:\n$projects_changed\n\nBoost Projects added or unpaused:\n$added_boost_programs\nBoost Projects removed or paused:\n$paused_boost_programs\nBoost Projects updated their program:\n$boost_projects_changed")
+	echo -e "$mg"
 
 	# Push to github
 	git add --all
@@ -164,6 +171,8 @@ else
 
 fi
 
+end_time=`date +%s`
+echo -e "Total run time = $(expr $end_time - $start_time) seconds\n"
 echo "#################################`date`#######################################\n"
 
 exit
